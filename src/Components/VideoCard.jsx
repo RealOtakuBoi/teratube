e30f6d02c1b3f18b395b9ef55671bf30f3f37d59
RealOtakuBoi <sohamseth2005@gmail.com>
2022-09-05 15:39:31 +0530

    Vidoeo thumbnails and title can now be seen

--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -1,9 +1,25 @@
+import { CheckCircle } from '@mui/icons-material'
+import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { demoChannelTitle, demoChannelUrl, demoThumbnailUrl, demoVideoTitle, demoVideoUrl } from '../Utils/Constants'
 
-const VideoCard = () => {
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+
   return (
-    <div>VideoCard</div>
+    <Card>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} 
+        sx={{ width: 358, height: 180 }}/>
+        </Link>
+        <CardContent sx = {{backgroundColor: '#1e1e1e' , height : '106px'}}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography variant='subtitle1' fontWeight='bold' color='#fff'>
+                {snippet?.title.slice(0,40) || demoVideoTitle.slice(0 , 40)}
+            </Typography>
+        </Link>
+        </CardContent>
+    </Card>
   )
 }
-
 export default VideoCard

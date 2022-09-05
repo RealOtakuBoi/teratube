63e3fe01df01b6dd980ae1e7c6cf9ffbf97cd0f0
RealOtakuBoi <sohamseth2005@gmail.com>
2022-09-05 12:47:18 +0530

    Blue Print done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 
 import './App.css';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import Navbar from './Components/Navbar';
+import Feed from './Components/Feed';
+import VideoDetail from './Components/VideoDetail';
+import ChannelDetail from './Components/ChannelDetail';
+import SearchDetail from './Components/SearchDetail';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+
+
+
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{backgroundColor : '#000'}}>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element ={<Feed />}/>
+        <Route path='/video/:id' element ={<VideoDetail />}/>
+        <Route path='/channel/:id' element ={<ChannelDetail />}/>
+        <Route path='/search/:searchTerm' element ={<SearchDetail />}/>
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
 
 export default App;
